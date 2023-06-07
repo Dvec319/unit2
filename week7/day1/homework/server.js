4af4b3d59f26a19aa9197d3fed55728ece94f826
mdelvecchio <mdelvecchio3119@gmail.com>
2023-06-06 23:08:15 -0400

    Magic 8 Ball express application created.

--- week7/day1/homework/server.js
+++ week7/day1/homework/server.js
@@ -14,6 +14,7 @@ const app = express()
 // Declare Routes and Routers
 ///////////////////////
 
+// Greetings
 app.get("/greeting", (req, res) => {
     res.send("Hello, stranger.")
 })
@@ -24,12 +25,46 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Oh, hey there ${name}! Good to see you.`)
 })
 
+// Tip Calculator
 app.get("/tip/:total/:tipPercentage", (req, res) => {
 
     const total = req.params.total
     const tipPercentage = req.params.tipPercentage
-    res.send(`Your tip will be ${total * (.01 * tipPercentage)}`)
+    res.send(`Your tip will be $${total * (.01 * tipPercentage)}.`)
     
+})
+
+// Magic 8 Ball
+app.get("/magic/:phrase", (req, res) => {
+    const phrase = req.params.phrase
+    const arr = [
+			'It is certain',
+			'It is decidedly so',
+			'Without a doubt',
+			'Yes definitely',
+			'You may rely on it',
+			'As I see it yes',
+			'Most likely',
+			'Outlook good',
+			'Yes',
+			'Signs point to yes',
+			'Reply hazy try again',
+			'Ask again later',
+			'Better not tell you now',
+			'Cannot predict now',
+			'Concentrate and ask again',
+			"Don't count on it",
+			'My reply is no',
+			'My sources say no',
+			'Outlook not so good',
+			'Very doubtful',
+		];
+    const eightBallResponse = arr[Math.floor(Math.random() * arr.length)]
+    res.send(`
+        <h1>${phrase}?</h1>
+        <h1>${eightBallResponse}.</h1>
+    `)
+
 })
 
 ///////////////////////////
