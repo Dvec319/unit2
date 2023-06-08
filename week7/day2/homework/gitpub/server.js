4f182369ec06f948a1693156cff2a0363677a98b
mdelvecchio <mdelvecchio3119@gmail.com>
2023-06-07 23:03:24 -0400

    linking the index and show .ejs and rendering an individual drink in the show view

--- week7/day2/homework/gitpub/server.js
+++ week7/day2/homework/gitpub/server.js
@@ -2,19 +2,19 @@
 // Setup - Import deps and create app object
 ////////////////////////
 
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const drinks = require("./models/drinks")
+require("dotenv").config() // loads the variables in the .env into the process.env object
+const express = require("express") // importing the express library
+const morgan = require("morgan") // importing the morgan library
+const drinks = require("./models/drinks") // return whatever drinks.js exports
 
-const PORT = process.env.PORT
-const app = express()
+const PORT = process.env.PORT // getting the port from our .env file
+const app = express() // express application
 
 //////////////////////
 // Declare Middleware
 //////////////////////
 
-app.use(morgan("dev"))
+app.use(morgan("dev")) // sets up our logging middleware
 
 
 ///////////////////////
@@ -25,14 +25,20 @@ app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
 
+// Index - Get - Lists all the drinks - /drinks
 app.get("/drinks", (req, res) => {
+    // render an ejs template with all the drinks
     res.render("index.ejs", {drinks})
 })
 
+// Show - Get - Shows one drink - drinks/:id
 app.get("/drinks/:id", (req, res) => {
+    // grab the id from the url
     const id = req.params.id
+    // create a variable with the fruit specified
     const drink = drinks[id]
-    res.send(id)
+    // render a template with the fruit
+    res.render("show.ejs", {drink})
 })
 
 ///////////////////////////
