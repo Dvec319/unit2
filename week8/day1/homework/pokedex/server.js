5d3f3c882f53c08db159d6c3e27ec9cb5a1a3d0c
mdelvecchio <mdelvecchio3119@gmail.com>
2023-06-16 02:16:39 -0400

    got new and edit pages working and everything seems to be running fine

--- week8/day1/homework/pokedex/server.js
+++ week8/day1/homework/pokedex/server.js
@@ -30,27 +30,68 @@ app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {allPokemon})
 })
 
-// New - Get - Show a form to make a new Pokemon
+// New - Get - Show a form to make a new Pokemon - /pokemon/new
 app.get('/pokemon/new', (req, res) => {
     res.render("new.ejs")
 })
 
-// Destroy - Delete - Delete a Pokemon
+// Destroy - Delete - Delete a Pokemon - /pokemon/:id
 app.delete('/pokemon/:id', (req, res) => {
     const id = req.params.id
     allPokemon.splice(id, 1)
     res.redirect('/pokemon')
 })
 
-// Update - Put - Update a Pokemon
+// Update - Put - Update a Pokemon - /pokemon/:id
+app.put('/pokemon/:id', (req, res) => {
+    const id = req.params.id
+    let updatedPokemon = {...allPokemon[id]}
+    updatedPokemon.id = req.body.id
+    updatedPokemon.name = req.body.name
+    updatedPokemon.img = req.body.img
+    let type = req.body.type;
+	let typeArray = type.split(" ");
+	updatedPokemon.type = typeArray;
+    updatedPokemon.stats = {
+		hp: req.body.hp,
+		attack: req.body.attack,
+	    defense: req.body.defense,
+		spattack: req.body.spattack,
+		spdefense: req.body.spdefense,
+		speed: req.body.speed,
+	};
+    allPokemon[id] = updatedPokemon
+    res.redirect('/pokemon')
+})
 
-// Create - Post - Create a Pokemon
+// Create - Post - Create a Pokemon - /pokemon
 app.post("/pokemon", (req, res) => {
-    allPokemon.push(req.body)
+    let newPokemon = {
+        name: req.body.name,
+        id: req.body.id,
+        img: req.body.img
+    }
+    let type = req.body.type
+    let typeArray = type.split(" ")
+    newPokemon.type = typeArray
+    newPokemon.stats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        spattack: req.body.spattack,
+        spdefense: req.body.spdefense,
+        speed: req.body.speed
+    }
+    allPokemon.unshift(newPokemon)
     res.redirect("/pokemon")
 })
 
-// Edit - Get - Render form to update a Pokemon
+// Edit - Get - Render form to update a Pokemon - /pokemon/:id/edit
+app.get('/pokemon/:id/edit', (req, res) => {
+    const id = req.params.id
+    const pokemon = allPokemon[id]
+    res.render('edit.ejs', {pokemon, id})
+})
 
 // Show - Get - Show one Pokemon - /pokemon/:id
 app.get('/pokemon/:id',  (req, res) => {
