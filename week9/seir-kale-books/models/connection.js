d3e3a6e7b827dd377d7f8845815c74bf03f55671
mdelvecchio <mdelvecchio3119@gmail.com>
2023-06-24 10:51:44 -0400

    Pushing up classwork

--- week9/seir-kale-books/models/connection.js
+++ week9/seir-kale-books/models/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL);
 
-mongoose.connection;
+mongoose.connection
 // .on('error', (err) => console.log(err.message))
 // .on('connected', () => console.log('connected'))
 // .on('disconnected', () => console.log('disconnected'))
